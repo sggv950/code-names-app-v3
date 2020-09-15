f710dbf435d4f3de0962067a57e96c409b089d2a
sggv950 <38014323+sggv950@users.noreply.github.com>
2020-09-15 15:33:13 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import styled from "styled-components";
+import "./App.css";
+
+interface MyState {
+  name: string;
+  version: number;
+}
+
+export const NiceHeadline = styled.h1`
+  padding: 10px;
+  background-color: #fff;
+  color: #111;
+  border-radius: 10px;
+`;
 
 function App() {
+  const [exampleState, updateExampleState] = useState<MyState>({
+    name: "code names app",
+    version: 3,
+  });
+
   return (
     <div className="App">
       <header className="App-header">
+        <NiceHeadline>
+          name: {exampleState.name} - version: {exampleState.version}
+        </NiceHeadline>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
