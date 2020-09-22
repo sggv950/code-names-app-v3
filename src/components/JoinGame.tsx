3cd2bbac724702f40b5f1c41db07042644da24e2
taldwek <64541444+taldwek@users.noreply.github.com>
2020-09-22 16:38:10 +0300

    added basic homepage layout, id fetch from json-server

---
+++ src/components/JoinGame.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const JoinGame = () => {
+   
+  return (
+    <div>
+        Join Game Placeholder
+    </div>
+  );
+}
+
+export default JoinGame
---
+++ src/pages/NavBar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NavBar = () => {
+
+
+    
+  return (
+    <div>
+ NavBar Placeholder
+    </div>
+  );
+}
+
+export default NavBar
