d0a66192c0d3e85e21db8f901b01865bf22d7d58
taldwek <64541444+taldwek@users.noreply.github.com>
2020-09-23 13:58:50 +0300

    adding stuff

--- src/components/NewGame.tsx
+++ src/components/NewGame.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from "react-router-dom"
+import axios from 'axios'
+import styled from 'styled-components'
+
+const Button = styled.button`
+font-family: sans-serif;
+border-radius: 5px;
+border: none;
+`
+
 
 const NewGame = () => {
-   
+  
+  
+  const [id, setId] = useState('');
+    
+  const getId = async () => {
+  const { id } = await (await axios.get('http://localhost:3000/game')).data
+  setId(id)
+  }
+
+
   return (
     <div>
-        New Game Placeholder
+      <div>
+        <label htmlFor="">Name</label>
+        <input type="text" name="" id=""/>
+      </div>
+      <Button onClick={getId}>Create New Game</Button>
+      <div>
+        {id && <Link to={`/game/${id}`}>Navigate to Page</Link>}
+      </div>
     </div>
   );
 }
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,26 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import NewGame from '../components/NewGame'
 import JoinGame from '../components/JoinGame'
-import axios from 'axios'
-// import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 
 
 const Homepage = () => {
 
-    const [id, setId] = useState('');
-
-    useEffect(() => {
-        const getId = async () => {
-        const id = await (await axios.get('http://localhost:3000/game')).data.id
-        setId(id)
-        }
-        getId()
-    })
-
-    
   return (
     <div>
-        current id : {id}
+    
       <NewGame />
       <JoinGame />
     </div>
